23aab57963a047367fa0c792a7fa1f6776535230
Sebastian Weigelt <weigelt@kit.edu>
2021-03-19 18:47:25 +0100

    moved multiclass label to new package

--- src/main/java/edu/kit/ipd/pronat/teaching/MulticlassLabel.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.ipd.pronat.teaching;
-
-/**
- * @author Sebastian Weigelt
- * @author Vanessa Steurer
- */
-public enum MulticlassLabel {
-	DECL, DESC, ELSE
-}
---
+++ src/main/java/edu/kit/ipd/pronat/teaching/classifier/MulticlassLabel.java
@@ -0,0 +1,33 @@
+package edu.kit.ipd.pronat.teaching;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Sebastian Weigelt
+ * @author Vanessa Steurer
+ */
+public enum MulticlassLabel {
+	DECL(1), DESC(2), ELSE(3);
+
+	private final int id;
+	public static final Map<Integer, MulticlassLabel> LabelLookup = new HashMap<>();
+
+	static {
+		for (MulticlassLabel l : MulticlassLabel.values()) {
+			LabelLookup.put(l.getId(), l);
+		}
+	}
+
+	MulticlassLabel(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public static Map<Integer, MulticlassLabel> getLabelLookup() {
+		return LabelLookup;
+	}
+}
